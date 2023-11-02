d2203b326b3ebb2ab47f51ff3bbbc706a8fa883d
mufaqar <mufaqar@gmail.com>
2023-11-02 18:41:37 +0500

    updated

--- components/Common/MaskImage.jsx
+++ components/Common/MaskImage.jsx
@@ -1,10 +1,10 @@
 export default function MaskImage({ url }) {
   return (
     <div className="box group">
-      <img src="/img.png" className="main " />
-      <img src="/after.png" className="firstlayer  group-hover:animate-pulse" />
+      <img src="/img.png" className="main w-full md:w-[85%] " />
+      <img src="/after.png" className="firstlayer  group-hover:animate-pulse hidden md:block" />
       <figure>
-        <img src="/before.png" className="before " />
+        <img src="/before.png" className="before hidden md:block" />
       </figure>
     </div>
   );
