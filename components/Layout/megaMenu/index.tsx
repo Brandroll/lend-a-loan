08cf5de2713e9616c32efca3f670029788e331cc
sued-zunair-gillani <zunairgillani54@gmail.com>
2024-02-18 12:59:03 +0500

    update

--- components/Layout/megaMenu/index.tsx
+++ components/Layout/megaMenu/index.tsx
@@ -9,7 +9,7 @@ const MegaMenu = ({setMegaMenu}:any) => {
                     <nav id="megamenu" className="border-b pt-4 z-50 fixed top-[60px] right-0 left-0 bg-white border-gray-100 lg:shadow-xl">
                          <div className='border-t-[1px] border-gray-100'/>
                          <div
-                              className="grid py-4 px-4 mx-auto max-w-screen-xl text-gray-800 md:grid-cols-3 lg:grid-cols-4 md:px-6">
+                              className="grid py-10 px-4 mx-auto max-w-screen-xl text-gray-800 md:grid-cols-3 lg:grid-cols-4 md:px-6">
                               {
                                    MenuItems?.map((item, idx) => (
                                         <ul className="col-span-2 md:col-span-1" key={idx}>
@@ -17,12 +17,12 @@ const MegaMenu = ({setMegaMenu}:any) => {
                                                   item.column.map((l, i) => (
                                                        <li key={i} onClick={()=>setMegaMenu(false)} >
                                                             <Link href={l.href} className="flex p-3 rounded-lg hover:bg-[#61C0CD] hover:text-white">
-                                                                 {/* <svg className="mr-2 w-6 h-6" fill="currentColor" viewBox="0 0 20 20"
+                                                                 <svg className="mr-2 w-6 h-6" fill="currentColor" viewBox="0 0 20 20"
                                                                       xmlns="http://www.w3.org/2000/svg">
                                                                       <path fill-rule="evenodd"
                                                                            d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
                                                                            clip-rule="evenodd"></path>
-                                                                 </svg> */}
+                                                                 </svg>
                                                                  <div>
                                                                       <div className={`${l.bold ? 'font-semibold' : 'font-isidorasans_regular'}`}>{l.label}</div>
                                                                       
@@ -36,7 +36,7 @@ const MegaMenu = ({setMegaMenu}:any) => {
                                    ))
                               }
                               <div className='relative hidden lg:block'>
-                                   <Image src={`/imgs/businessman-corporate-valuation-advisors-cva-10.png`} className='absolute -bottom-4 right-0' alt="img" width={200} height={400}/>
+                                   <Image src={`/imgs/businessman-corporate-valuation-advisors-cva-10.png`} className='absolute -bottom-10 right-0' alt="img" width={200} height={400}/>
                               </div>
                          </div>
                     </nav>
